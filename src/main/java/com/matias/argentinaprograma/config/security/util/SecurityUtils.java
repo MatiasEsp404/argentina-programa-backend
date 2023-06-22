8baf34f8ad7253419f6171b679fb15544869d727
MatiasEsp404 <unity.matias@outlook.com>
2023-06-22 20:06:25 -0300

    Keep it simple, stupid

--- src/main/java/com/matias/argentinaprograma/config/security/util/SecurityUtils.java
+++ src/main/java/com/matias/argentinaprograma/config/security/util/SecurityUtils.java
@@ -1,5 +1,6 @@
 package com.matias.argentinaprograma.config.security.util;
 
+import com.matias.argentinaprograma.config.security.common.Role;
 import com.matias.argentinaprograma.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -7,29 +8,31 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import com.matias.argentinaprograma.config.security.common.Role;
-
 @Component
 public class SecurityUtils {
 
-	@Autowired
-	private IUserRepository userRepository;
+  @Autowired
+  private IUserRepository userRepository;
 
-	public Authentication getAuthentication() {
-		return SecurityContextHolder.getContext().getAuthentication();
-	}
+  public Authentication getAuthentication() {
+    return SecurityContextHolder.getContext().getAuthentication();
+  }
 
-	public UserDetails getUserAuthenticated() {
-		Object principal = getAuthentication().getPrincipal();
-		String username = (principal instanceof UserDetails) ? ((UserDetails) principal).getUsername()
-				: principal.toString();
+  public UserDetails getUserAuthenticated() {
+    Object principal = getAuthentication().getPrincipal();
+    String username;
+    if (principal instanceof UserDetails) {
+      username = ((UserDetails) principal).getUsername();
+    } else {
+      username = principal.toString();
+    }
+    return userRepository.findByEmail(username);
+  }
 
-		return userRepository.findByEmail(username);
-	}
-
-	public boolean hasAdminRole() {
-		return getAuthentication().getAuthorities().stream()
-				.anyMatch(grantedAuthority -> Role.ADMIN.getFullRoleName().equals(grantedAuthority.getAuthority()));
-	}
+  public boolean hasAdminRole() {
+    return getAuthentication().getAuthorities().stream()
+        .anyMatch(grantedAuthority -> Role.ADMIN.getFullRoleName()
+            .equals(grantedAuthority.getAuthority()));
+  }
 
 }
